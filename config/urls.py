975c2badea6cc6af32809e5eccc65c84b5adb424
PaulyDuk <me@paulyd.co.uk>
2025-07-24 11:09:46 +0100

    Added games, publishers and developers to admin.py, verified working OK in admin panel

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from reviews.views import my_reviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('', my_reviews, name='my_reviews'),
 ]
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
-
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Review, Publisher, Developer
 # Register your models here.
+
+class ReviewInline(admin.TabularInline):
+    """Inline display of reviews for Publisher and Developer"""
+    model = Review
+    extra = 0  # Don't show extra empty forms
+    fields = ('title', 'genre', 'console', 'review_score', 'is_published', 'is_featured', 'created_on')
+    readonly_fields = ('created_on',)
+    show_change_link = True  # Allows clicking through to edit the full review
+
+@admin.register(Publisher)
+class PublisherAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description',)
+    list_display = ('name', 'founded_year', 'headquarters', 'created_on')
+    list_filter = ('founded_year', 'created_on')
+    search_fields = ('name',)
+    inlines = [ReviewInline]  # Shows all games published by this publisher
+
+
+@admin.register(Developer)
+class DeveloperAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description',)
+    list_display = ('name', 'founded_year', 'headquarters', 'created_on')
+    list_filter = ('founded_year', 'created_on')
+    search_fields = ('name',)
+    inlines = [ReviewInline]  # Shows all games developed by this developer
+
+
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description', 'review_text')
+    list_display = ('title', 'publisher', 'developer', 'genre', 'console', 'review_score', 'is_published')
+    list_filter = ('genre', 'console', 'is_published', 'publisher', 'developer')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
